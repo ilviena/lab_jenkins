81a006d834a0b2014dc9000cfaa7c376825d1e6b
Ильвина Ямалетдинова <yamaletdinova.ilvina@gmail.com>
2025-11-22 12:18:31 +0500

    Update app.py

--- app.py
+++ app.py
@@ -9,5 +9,5 @@ def hello():
     return render_template('index.html', student_name=student_name)
 
 if __name__ == '__main__':
-    port = int(os.getenv('PORT', 8083))
+    port = int(os.getenv('PORT', 8080))
     app.run(host='0.0.0.0', port=port)
